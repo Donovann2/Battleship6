a6d33b344abb96b73784b17f515bba86146af632
itssmepaul12 <therche1234@gmail.com>
2023-07-14 08:38:08 -0400

    made a lots of changes to the code

--- battleship.py
+++ battleship.py
@@ -1,68 +1,55 @@
 #Made by Don and Paul
-import random
+from random import randint
 
-# This area right here....is to create the board.
-def create_board():
-    board = []
-    for _ in range(5):
-        board.append(["O"] * 5)
-    return board
+board = []
 
-#This area right here.....is to print the game board.
+for x in range(5):
+    board.append(["O"] * 5)
+
 def print_board(board):
     for row in board:
         print((" ").join(row))
 
-#This area right here fam......is to place the battleships on the board.
-def place_battleships(board):
-    for _ in range(3):
-        row = random.randint(0, 4)
-        col = random.randint(0, 4)
-        board[row][col] = "B"
+print("Let's play Battleship!")
+print_board(board)
 
-#This right here fam....is to get user's guess and check 
-def guess_ship(board):
-    while True:
-        guess_row = int(input("Guess Row (0-4): "))
-        guess_col = int(input("Guess Row (0-4): "))
-        if guess_row < 0 or guess_row > 4 or guess_col < 0 or guess_col > 4:
-            print("Nah fam. You should have known that's not even in the ocean...OP.")
-        elif board[guess_row][guess_col] == "X":
-            print("Fam, you guessed that one already. bruh")
-        else:
-            break
 
-    if board[guess_row][guess_col] = == "B":
-        print("Congrats fam. you sank the battleship.")
-        board[guess_row][guess_col] = "X"
-        return True
-    else:
+def random_row(board):
+    return randint(0, len(board) - 1)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(9):
+    print ("Turn"), turn
+    guess_row = int(input("Guess Row (0-4):"))
+    guess_col = int(input("Guess Col (0-4):"))
+
+    if guess_row == ship_row and guess_col == ship_col:
         print("bruh, you are an op. you missed the battleship.")
-        board[guess_row][guess_col] = "X"
-        return False
-
-#This right here fam....is to play the game....NERD
-def play_battleship():
-    print("Let's play some battleship...or whatever fam.....")
-    board = create_board()
-    place_battleships(board)
-    print_board(board)
-    
-    while True:
-        if guess_ship(board):
-            print_board(board)
-            play_again = input("fam, do you want to play again bruh? (yes/no): ")
-            if play_again.lower() == "no":
-                print("thanks for playing fam. bye op.")
-                break
-            else:
-                board = create_board()
-                place_battleships(board)
-                print_board(board)
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print("Nah fam. You should have known that's not even in the ocean...OP.")
+        elif(board[guess_row][guess_col] == "X"):
+            print("Fam, you guessed that one already. bruh.")
         else:
+            print("bruh, you are an op. you missed the battleship.")
+            board[guess_row][guess_col] = "X"
+    if turn == 2:
+        print("Game Over")
+        while True:
+            answer = str(input('Do you wanna play again, fam? (yes/no): '))
+            if answer in ('yes', 'no'):
+                break
+            print("Invaild input, please type the correct input.")
+        if answer == 'yes':
             print_board(board)
-
-play_battleship()
-    
-
-
+        
+        else:
+            print("Bye op.")
+            break
+    turn =+ 1
+    print_board(board)
